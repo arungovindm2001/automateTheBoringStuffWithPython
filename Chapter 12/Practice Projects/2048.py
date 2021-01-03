1feed77d732d7b032aa30507805c01336101cd08
arungovindm2001 <arungovindm@gmail.com>
2021-01-04 01:29:35 +0530

    Added 2048 project

---
+++ Chapter 12/Practice Projects/2048.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+browser = webdriver.Chrome()
+browser.get('https://gabrielecirulli.github.io/2048/')
+print('Entered the site')
+htmlElem = browser.find_element_by_tag_name('html')
+
+while True:
+    htmlElem.send_keys(Keys.UP)
+    htmlElem.send_keys(Keys.RIGHT)
+    htmlElem.send_keys(Keys.DOWN)
+    htmlElem.send_keys(Keys.LEFT)
